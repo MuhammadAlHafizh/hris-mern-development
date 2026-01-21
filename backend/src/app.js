9d7dff5139a742caa465b7e8ab7fda779a572f12
Muhammad Al Hafizh <apit637cnr@gmail.com>
2026-01-21 22:41:15 +0700

    Update app.js
    update api

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,8 @@ app.use(cors({
 
     const allowedOrigins = [
       "http://localhost:5173",
-      "http://192.168.1.7:5173"
+      "http://192.168.1.7:5173",
+      "https://hris.nuncorp.id",
     ];
 
     if (allowedOrigins.includes(origin)) {
